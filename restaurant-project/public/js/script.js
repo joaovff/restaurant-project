4a689294a7504d97cc49718145c3e88c884e5c0d
João Figueiredo Silva <you@example.com>
2022-11-27 20:31:20 +0000

    maps api done

--- restaurant-project/public/js/script.js
+++ restaurant-project/public/js/script.js
@@ -3,3 +3,41 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("restaurant-project JS imported successfully!");
 });
 
+function startMap() {
+  const ironhackBCN = {
+    lat: 38.711798333387264,
+    lng: -9.124110649886294,
+  };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: ironhackBCN,
+  });
+  const myMarker = new google.maps.Marker({
+    position: {
+      lat: 38.711798333387264,
+      lng: -9.124110649886294,
+    },
+    map: map,
+    title: "I'm here",
+  });
+}
+
+function toggleFAB(fab) {
+  if (document.querySelector(fab).classList.contains("show")) {
+    document.querySelector(fab).classList.remove("show");
+  } else {
+    document.querySelector(fab).classList.add("show");
+  }
+}
+
+document.querySelector(".fab .main").addEventListener("click", function () {
+  toggleFAB(".fab");
+});
+
+document.querySelectorAll(".fab ul li button").forEach((item) => {
+  item.addEventListener("click", function () {
+    toggleFAB(".fab");
+  });
+});
+
+startMap();
