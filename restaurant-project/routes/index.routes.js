5e8a3a1e39f7f9862349ded28fc3e2af9315f8a0
João Figueiredo Silva <you@example.com>
2022-12-02 14:05:01 +0000

    nodemailer added

--- restaurant-project/routes/index.routes.js
+++ restaurant-project/routes/index.routes.js
@@ -2,6 +2,38 @@ const express = require("express");
 const router = express.Router();
 const Dish = require("../models/Dish.model");
 const { all } = require("./dishes.routes");
+const nodemailer = require("nodemailer");
+
+router.post("/send-mail", async (req, res, next) => {
+  const { name, email, date, people, time } = req.body;
+
+  let transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "97631978183395",
+      pass: "1a97c4d3122f37",
+    },
+  });
+  let message = {
+    from: "noreplay@idea.pt",
+    to: "books@idea.pt",
+    subject: `Reservation for:${date},${time}`,
+    text: `Name: ${name}, Email: ${email}, Table for: ${people} Date:${date}, Time:${time}`,
+    html: "<p>HTML version of the message</p>",
+  };
+
+  transport.sendMail(message, (err) => {
+    if (err) {
+      return res.status(404).json({
+        erro: true,
+        message: "Failed to send the email!",
+      });
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
 /* GET home page */
 router.get("/", async (req, res, next) => {
